628f5bafc2a22c2338e780bd12d5f20569ab27a9
Raphael Wanderley <rapha.rw@gmail.com>
2020-10-02 14:48:14 -0300

    Including security

---
+++ app/plugins/helmet-support.js
@@ -0,0 +1,23 @@
+const fp = require("fastify-plugin");
+const helmet = require("fastify-helmet");
+
+module.exports = fp(async (fastify, options) => {
+  fastify.register(helmet, {
+    hidePoweredBy: { setTo: "none" },
+    contentSecurityPolicy: {
+      directives: {
+        scriptSrc: [
+          "'self'",
+          "'sha256-iV83EgAQc1+Q++O7L1ZemfWFbYYPNv2syB2HreE5S/8='" /* -> permiss swagger script*/,
+        ],
+        imgSrc: ["'self'", "data:" /* -> permiss swagger img data:*/],
+        defaultSrc: [
+          "'self'",
+          "'sha256-pyVPiLlnqL9OWVoJPs/E6VVF5hBecRzM2gBiarnaqAo='" /* -> permiss swagger defaultScript*/,
+        ],
+        objectSrc: ["'self'"],
+        upgradeInsecureRequests: [],
+      },
+    },
+  });
+});
